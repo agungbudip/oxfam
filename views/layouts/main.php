b4612a865fda298093e66c4dc5e6ff068514af7d
agung <agungbudi3@gmail.com>
2016-03-09 17:05:02 +0700

    perlengkapan awal

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -23,7 +23,7 @@ AppAsset::register($this);
         <div class="wrapper">
             <header class="main-header">
                 <!-- Logo -->
-                <a href="#" class="logo">
+                <a href="<?= yii\helpers\Url::home() ?>" class="logo">
                     <!-- mini logo for sidebar mini 50x50 pixels -->
                     <span class="logo-mini"><b>OXF</b></span>
                     <!-- logo for regular state and mobile devices -->
